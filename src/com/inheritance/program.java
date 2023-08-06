c8c3584cb41db90f4295aecbcc1ef999ec9b7278
koormaavadharam <vishalrajagopalan22@gmail.com>
2023-08-06 19:05:53 +0530

    new code

---
+++ src/com/inheritance/program.java
@@ -0,0 +1,19 @@
+package com.inheritance;
+
+public class program {
+	
+	public  final void age() {
+		
+		System.out.println("age is 50");
+	}
+	
+	public void gender() {
+		
+		System.out.println("gender is female");
+	}
+	
+	
+
+}
+
+
---
+++ src/com/inheritance/program1.java
@@ -0,0 +1,34 @@
+package com.inheritance;
+
+public class program1 extends program {
+	
+	public void Age() {
+		
+		System.out.println("son age is 15");
+		
+	}
+	
+	
+		
+	
+public void number() {
+		
+		System.out.println("number is 5");
+		
+	}
+	
+			public static void main(String[] args){
+				
+				program1 p=new program1();
+				p.age();
+				p.number();
+		
+		
+					
+		}
+	}
+
+
+
+
+
